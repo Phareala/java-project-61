02b0db96a522f5f95b8de3f0cd57c418d63f378f
Phareal <pashaosadchii@mail.ru>
2023-02-20 20:05:03 +0300

    Добавил новую игру в меню

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,18 +1,22 @@
 package hexlet.code;
 
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n0 - Exit");
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
         switch (choice) {
             case  (1):
                 Cli.greeting();
                 break;
+            case (2):
+                Even.evenGame();
             default:
                 break;
         }
