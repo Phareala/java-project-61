5398731db2bdd18fc1d6e3448b789a72717231cf
Phareal <pashaosadchii@mail.ru>
2023-02-20 11:59:44 +0300

    Добавили меню выбора

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,20 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.askName();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case  (1):
+                Cli.greeting();
+                break;
+            default:
+                break;
+        }
     }
 }
