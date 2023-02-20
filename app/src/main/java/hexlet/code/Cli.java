074a4c265452734bc65ca8c5ebc9771c3787fe90
Phareal <pashaosadchii@mail.ru>
2023-02-20 11:58:37 +0300

    Перенесли приветствие в отдельный класс

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,8 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void askName() {
+    public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = scanner.next();
