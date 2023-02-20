e3e0d76aa523d17366704c0ee71cea3df67fa8a8
Phareal <pashaosadchii@mail.ru>
2023-02-20 20:02:51 +0300

    Добавил метод получения имени

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,16 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+    static String userName;
     public static void greeting() {
         System.out.println("Welcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + '!');
+        String clientName = scanner.next();
+        System.out.println("Hello, " + clientName + '!');
+        userName = clientName;
+    }
+    public static String getName() {
+        return userName;
     }
 }
