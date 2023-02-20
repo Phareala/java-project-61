25c99927e2ca57b922fe31bd9e08478ee8717de3
Phareal <pashaosadchii@mail.ru>
2023-02-20 22:16:26 +0300

    Небольшие правки в логике игры

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -9,31 +9,27 @@ public class Even {
         Scanner scanner = new Scanner(System.in);
         Cli.greeting();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        var checker = true;
         var i = 0;
-        while(i < 3) {
+        while (i < 3) {
             String answer;
-            if(checker) {
-                int number = (int) (Math.random() * 10);
-                if(number % 2 == 0) {
-                    answer = "yes";
-                } else {
-                    answer = "no";
-                }
-                System.out.println("Question: " + number);
-                System.out.print("Your answer: ");
-                String cliAnswer = scanner.next();
-                if (cliAnswer.equals(answer)) {
-                    System.out.println("Correct!");
-                    i++;
-                } else {
-                    System.out.println(cliAnswer + " is wrong answer ;(. Correct answer was " + answer);
-                    checker = false;
-                }
-            if(checker) {
-                System.out.println("Congratulations " + Cli.getName() + '!');
+            int number = (int) (Math.random() * 10);
+            if (number % 2 == 0) {
+                answer = "yes";
+            } else {
+                answer = "no";
             }
+            System.out.println("Question: " + number);
+            System.out.print("Your answer: ");
+            String cliAnswer = scanner.next();
+            if (cliAnswer.equals(answer)) {
+                System.out.println("Correct!");
+                i++;
+            } else {
+                System.out.println("'" + cliAnswer + "'" + " is wrong answer ;(. Correct answer was " + "'" + answer + "'.");
+                System.out.println("Let's try again, " + Cli.getName() + '!');
+                break;
             }
+            System.out.println("Congratulations, " + Cli.getName() + '!');
         }
     }
 }
