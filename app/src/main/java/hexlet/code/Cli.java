0daa1bbd60d5c5b13f9ed384d1525c1ff67df496
Phareal <pashaosadchii@mail.ru>
2023-02-20 11:36:14 +0300

    Fix checkstyle errors

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void askName(){
+    public static void askName() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = scanner.next();
