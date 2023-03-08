bf9af269e588318cc1a5dd908df0d5fb2f86d3b0
Phareal <pashaosadchii@mail.ru>
2023-03-08 20:30:23 +0300

    Добавление новой игры в меню запуска

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,7 @@ import hexlet.code.core.Engine;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 
@@ -12,13 +13,12 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet\n2 - Even\n3 - Calc");
-        System.out.println("4 - GCD");
+        System.out.println("4 - GCD\n5 - Progression");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
         switch (choice) {
-            case (1) ->
-                    Cli.greeting();
+            case (1) -> Cli.greeting();
             case (2) -> {
                 var game = new Even();
                 new Engine(game).launchGame();
@@ -29,6 +29,10 @@ public class App {
             }
             case (4) -> {
                 var game = new GCD();
+                new Engine(game).launchGame();
+            }
+            case (5) -> {
+                var game = new Progression();
                 new Engine(game).launchGame();
             }
             default -> System.out.println("Goodbye");
